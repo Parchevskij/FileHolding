7308b618e1f7265c80f8b03d3e4cd1469f8c485d
BezymiannyiOlexii <genguly@gmail.com>
2021-06-10 15:19:39 +0300

    apload file from flask to data

--- test_file.py
+++ test_file.py
@@ -3,9 +3,6 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-HOST = "127.0.0.1"
-PORT = 65431
-
 
 @app.route('/')
 def index():
@@ -14,7 +11,11 @@ def index():
 
 @app.route('/', methods=['POST'])
 def upload_file():
+    HOST = "127.0.0.1"
+    PORT = 65431
     uploaded_file = request.files['file']
+    raw = uploaded_file.read()
+    '''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         raw = uploaded_file.read()
@@ -23,7 +24,19 @@ def upload_file():
         s.sendall(uploaded_file.filename.encode('ascii'))
         s.sendall(len(raw).to_bytes(8, 'big'))
         s.sendall(raw)
+    '''
 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        print("Client Sending flag")
+        flag: int = 1
+        s.send(flag.to_bytes(1, 'big'))
+        print("Client Sending:", uploaded_file.filename)
+
+        s.sendall(len(uploaded_file.filename).to_bytes(4, 'big'))
+        s.sendall(uploaded_file.filename.encode('ascii'))
+        s.sendall(len(raw).to_bytes(8, 'big'))
+        s.sendall(raw)
     print("Received")
     return redirect(url_for('index'))
 
--- test_file2.py
+++ test_file2.py
@@ -1,36 +1,52 @@
 import socket
+
 '''
-Server part
+    Server
 '''
 
 HOST = "127.0.0.1"
 PORT = 65431
+filename = 'test.pdf'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-
     while True:
         conn, addr = s.accept()
-        with conn:
-            print("Connected by", addr)
-            expected_filesize = conn.recv(4)
-            filesize = int.from_bytes(expected_filesize, 'big')
-            filename = conn.recv(filesize).decode()
-            expected_size = b""
+        print("Connected by", addr)
+        print("Server receiving flag")
+        val = int.from_bytes(conn.recv(1), 'big')
+        print(val)
+        if val == 1:
+            print('Server Receiving')
+            with conn:
+                expected_filesize = conn.recv(4)
+                filesize = int.from_bytes(expected_filesize, 'big')
+                filename = conn.recv(filesize).decode()
+                expected_size = b""
+                while len(expected_size) < 8:
+                    more_size = conn.recv(8 - len(expected_size))
+                    if not more_size:
+                        raise Exception("Short file length received")
+                    expected_size += more_size
+                expected_size = int.from_bytes(expected_size, 'big')
 
-            while len(expected_size) < 8:
-                more_size = conn.recv(8 - len(expected_size))
-                if not more_size:
-                    raise Exception("Short file length received")
-                expected_size += more_size
-            expected_size = int.from_bytes(expected_size, 'big')
-            packet = b""  # Use bytes, not str, to accumulate
-            while len(packet) < expected_size:
-                buffer = conn.recv(expected_size - len(packet))
-                if not buffer:
-                    raise Exception("Incomplete file received")
-                packet += buffer
+                packet = b""
+                while len(packet) < expected_size:
+                    buffer = conn.recv(expected_size - len(packet))
+                    if not buffer:
+                        raise Exception("Incomplete file received")
+                    packet += buffer
+                filename = 'data/new_'+filename
+                with open(filename, 'wb') as f:
+                    f.write(packet)
+        else:
+            print("Server Sending")
+            with conn:
+                with open('data/' + filename, 'rb') as f:
+                    raw = f.read()
 
-            with open(filename, 'wb') as f:
-                f.write(packet)
+                conn.sendall(len(filename).to_bytes(4, 'big'))
+                conn.sendall(filename.encode())
+                conn.sendall(len(raw).to_bytes(8, 'big'))
+                conn.sendall(raw)
